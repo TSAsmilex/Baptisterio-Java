e1c147496b1019830f02006e537c624dc639365f
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-13 12:07:59 +0200

    Revert "Base de datos de usuarios"
    This reverts commit cd9a56baa3aaaad000b5bee09d1a389a45a2a5b7.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -7,17 +5,13 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import javax.security.auth.login.LoginException;
-
 
 enum MenuOptions {
     NOOP(0),
     BOOK(1),
     CANCEL(2),
     LIST(3),
-    REGISTER_USER(4),
-    LOGIN_USER(5),
-    EXIT(6);
+    EXIT(4);
 
     private final int value;
 
@@ -43,19 +37,17 @@ enum MenuOptions {
         System.out.println("   1) Reservar entradas");
         System.out.println("   2) Anular una reserva");
         System.out.println("   3) Listar una reserva");
-        System.out.println("   4) Registrarse");
-        System.out.println("   5) Login");
-        System.out.println("   6) Salir");
+        System.out.println("   4) Salir");
     }
 }
 
 
 public class App {
     static Baptisterio baptisterio = new Baptisterio();
-    static UserDB userDB           = new UserDB();
-    static User user               = null;
 
     public static void main(String[] args) throws Exception {
+        System.out.print("¿¿¿A quién no le va a gustar un buen baptisterio???");
+
         var       option = MenuOptions.NOOP;
         Scanner   scan   = new Scanner(System.in);
         Exception error  = null;
@@ -66,18 +58,13 @@ public class App {
             }
 
             if (error != null) {
-                System.err.println("Se ha producido un error. Motivo: " + error.getMessage());
+                System.err.println("Se ha producido un error. Motivo:\t" + error.getMessage());
             }
 
             error  = null;
             option = MenuOptions.NOOP;
 
-            if (user != null) {
-                System.out.println("Hola, " + user.getName() + ". ¿Qué quieres hacer?");
-            }
-            else {
-                System.out.println("\n¡Hola! ¿Qué quieres hacer?");
-            }
+            System.out.println("\n¡Hola! ¿Qué quieres hacer?");
             MenuOptions.printMenu();
 
             error = null;
@@ -100,41 +87,19 @@ public class App {
                     catch (DateTimeException e) {
                         error = e;
                     }
-                    catch (LoginException e) {
+                    catch (Exception e) {
                         error = e;
                     }
                 }
-                case CANCEL -> {
-                    try {
-                        cancel(scan);
-                    }
-                    catch (LoginException e) {
-                        error = e;
-                    }
-                }
-                case LIST            -> baptisterio.printReservations();
-                case REGISTER_USER   -> registerUser(scan);
-                case LOGIN_USER      -> {
-                    try {
-                        login(scan);
-                    }
-                    catch (LoginException e) {
-                        error = e;
-                    }
-                }
-                case EXIT            -> System.out.println("¡Hasta luego!");
-                case NOOP            -> {}
+                case CANCEL -> cancel(scan);
+                case EXIT   -> System.out.println("¡Hasta luego!");
+                case LIST   -> baptisterio.printReservations();
+                case NOOP   -> {}
             }
         } while (option != MenuOptions.EXIT);
 
         scan.close();
-
-        if (!userDB.writeDB()) {
-            System.err.println("Ha habido errores escribiendo la base de datos");
-        }
     }
-
-
     private static LocalDateWrapper readDate(Scanner scan) throws DateTimeException{
         System.out.println("¿Qué día quieres comprobar? (YYYY-MM-DD)");
         System.out.print("> ");
@@ -153,28 +118,8 @@ public class App {
     }
 
 
-    private static boolean login (Scanner scan) throws LoginException {
-        System.out.println("¿Cuál es tu DNI?");
-        System.out.print("> ");
-        String dni = scan.nextLine();
-
-        System.out.println("Escriba su contraseña");
-        System.out.print("> ");
-        String password = scan.nextLine();
-        String hashedPassword = getSHA512(password);
-
-        try {
-            user = userDB.login(dni, hashedPassword);
-        }
-        catch (LoginException e) {
-            throw new LoginException("El usuario o la contraseña son incorrectos");
-        }
-
-        return true;
-    }
-
-    private static boolean registerUser(Scanner scan){
-        System.out.println("Escriba un usuario");
+    private static User readUser(Scanner scan){
+        System.out.println("¿Cuál es tu nombre?");
         System.out.print("> ");
         String name = scan.nextLine();
 
@@ -182,32 +127,12 @@ public class App {
         System.out.print("> ");
         String dni = scan.nextLine();
 
-        System.out.println("Escriba una contraseña");
-        System.out.print("> ");
-        String password = scan.nextLine();
-        String hashedPassword = getSHA512(password);
-
-        User possibleUser = new User(name, dni, hashedPassword);
-
-        boolean addedCorrectly = userDB.addUser(possibleUser);
-
-        if (!addedCorrectly) {
-            throw new InputMismatchException("El usuario ya existe");
-        }
-
-        user = possibleUser;
-
-        return addedCorrectly;
+        User user = new User(name, dni);
+        return user;
     }
 
 
-    private static void book (Scanner scan)
-        throws Exception, DateTimeException, LoginException
-    {
-        if (user == null) {
-            throw new LoginException("Debes iniciar sesión para reservar las entradas.");
-        }
-
+    private static void book (Scanner scan) throws Exception, DateTimeException {
         LocalDateWrapper date = null;
 
         try {
@@ -223,6 +148,8 @@ public class App {
         if (!baptisterio.avaliable(date)) {
             throw new Exception("La fecha introducida no está disponible");
         }
+
+        User user = readUser(scan);
 
         int round     = 0;
         int remaining = baptisterio.PRICE;
@@ -255,12 +182,7 @@ public class App {
         Thread.sleep(2000);
     }
 
-
-    private static void cancel (Scanner scan) throws InterruptedException, LoginException {
-        if (user == null) {
-            throw new LoginException("Debes iniciar sesión para reservar las entradas.");
-        }
-
+    private static void cancel (Scanner scan) throws InterruptedException {
         LocalDateWrapper date = null;
 
         try {
@@ -269,6 +191,8 @@ public class App {
         catch (DateTimeException e) {
             throw e;
         }
+
+        User user = readUser(scan);
 
         if (baptisterio.cancel(user, date)) {
             System.out.println("Su cita se ha cancelado con éxito!");
@@ -281,27 +205,8 @@ public class App {
     }
 
 
-    // ─────────────────────────────────────────────────────────────────────────────
-
-
     static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-    }
-
-
-    public static String getSHA512(String input){
-        String toReturn = null;
-
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-512");
-            digest.reset();
-            digest.update(input.getBytes("utf8"));
-            toReturn = String.format("%0128x", new BigInteger(1, digest.digest()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return toReturn;
     }
 }
--- src/User.java
+++ src/User.java
@@ -2,13 +2,10 @@ public class User {
     // dni, name
     private String dni;
     private String name;
-    private String hashedPassword;
 
-
-    public User(String name, String dni, String hashedPassword){
+    public User(String name, String dni){
         this.name = name;
         this.dni  = dni;
-        this.hashedPassword=hashedPassword;
     }
 
     public String getDni() {
@@ -18,18 +15,13 @@ public class User {
     public String getName() {
         return name;
     }
-    public String getHashedPassword() {
-        return hashedPassword;
-    }
+
     public void setDni(String dni) {
         this.dni = dni;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
     }
 
     @Override
@@ -43,17 +35,3 @@ public class User {
     }
 
 }
-
-/*
-
-
-public static void main(String[] argv) {
-
-	String inputValue = "this is an example";
-
-	// With the java libraries
-	String sha256 = getSHA256(inputValue);
-
-	System.out.println("The SHA-256 of \"" + inputValue + "\" is:");
-	System.out.println(sha256);
- */
